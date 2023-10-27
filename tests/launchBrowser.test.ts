21b75fc1c3b5d5fa2ef2ed8e0f1475fb488ec874
nagabushanv <nagabushan@vtestcorp.com>
2023-10-27 15:41:25 +0530

    first commit

---
+++ tests/launchBrowser.test.ts
@@ -0,0 +1,14 @@
+import { chromium } from '@playwright/test';
+import test, { describe } from "node:test";
+describe('launch Browser',() => {
+
+    test('Open playwright', async ()=>{
+        const browser = await chromium.launch({
+            headless: false
+        })
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://playwright.dev/docs/intro')
+        await browser.close()
+    })
+})
